26c748157da33fdd5f7a2104c5eebb8db8b3435b
ToanDinh0503 <106976894+ToanDinh0503@users.noreply.github.com>
2023-06-21 17:31:36 +0700

    Pong Game here

---
+++ src/GameFrame.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	GamePanel panel;
+	
+	GameFrame(){
+		panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Pong Game");
+		this.setResizable(false);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
+
+
+
+
+
+
+
+
